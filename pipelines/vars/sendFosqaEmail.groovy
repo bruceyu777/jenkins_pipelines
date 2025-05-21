539f2ed52da3d3da076f7dfbfacebde489b85421
Fosqa User <bruceyu777>
2025-05-21 16:02:54 -0700

    update sendFosqaEmail.groovy

--- pipelines/vars/sendFosqaEmail.groovy
+++ pipelines/vars/sendFosqaEmail.groovy
@@ -26,22 +26,33 @@ def call(Map args = [:]) {
     }
 
     // 2) Back on the agent: heredoc into test_email.py
-    node {  
-        withEnv(["SMTP_PW=${pw}"]) {
-            sh """
-                cat <<EOF | python3 /home/fosqa/resources/tools/test_email.py \\
-                  --to-addr ${args.to} \\
-                  --subject \"${subject.replace('\"','\\\\\"')}\" \\
-                  --body \"${body.replace('\"','\\\\\"')}\" \\
-                  --smtp-server ${smtpServer} \\
-                  --port ${port} \\
-                  ${useSsl? '--use-ssl' : ''} \\
-                  ${useTls? '--use-tls' : ''} \\
-                  --username ${username} \\
-                  --password-stdin
-                \$SMTP_PW
+    // 2) On the agent: write secret to a temp file via heredoc, call test_email.py, then delete it
+  node { 
+    withEnv(["SMTP_PW=${pw}"]) {
+      sh """
+        #!/usr/bin/env bash
+        set -uo pipefail
+
+        # Create a temp file in the workspace (Jenkins won’t log its contents)
+        cat <<EOF > secret.pw
+        \$SMTP_PW
 EOF
-            """
-        }
+
+        # Invoke email send using the file
+        python3 /home/fosqa/resources/tools/test_email.py \\
+          --to-addr ${args.to} \\
+          --subject \"${subject.replace('\"','\\\\\"')}\" \\
+          --body \"${body.replace('\"','\\\\\"')}\" \\
+          --smtp-server ${smtpServer} \\
+          --port ${port} \\
+          ${useSsl ? '--use-ssl' : ''} \\
+          ${useTls ? '--use-tls' : ''} \\
+          --username ${username} \\
+          --password-file secret.pw
+
+        # Clean up immediately
+        shred -u secret.pw || rm -f secret.pw
+      """
     }
+  }
 }
