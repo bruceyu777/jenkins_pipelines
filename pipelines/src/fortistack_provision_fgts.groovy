29dbca2812819daa2a1d090d3aa733e2827f67c1
Fosqa User <bruceyu777>
2025-05-21 16:06:03 -0700

    update fortistack_provision_fgts.groovy to have git pull

--- pipelines/src/fortistack_provision_fgts.groovy
+++ pipelines/src/fortistack_provision_fgts.groovy
@@ -48,12 +48,35 @@ pipeline {
                 }
             }
         }
+        
         stage('Check Docker') {
             steps {
                 echo "Checking Docker environment..."
                 sh 'docker ps'
                 sh 'docker-compose --version'
                 sh 'virsh -c qemu:///system list --all'
+            }
+        }
+
+        stage('GIT Pull') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'LDAP', usernameVariable: 'SVN_USER', passwordVariable: 'SVN_PASS')]) {
+                        // Local Git update
+                        echo "=== Step 1: Local Git update ==="
+                        def innerGitCmd = "sudo -u fosqa bash -c 'cd /home/fosqa/resources/tools && " +
+                                          "if [ -n \"\$(git status --porcelain)\" ]; then git stash push -m \"temporary stash\"; fi; " +
+                                          "git pull; " +
+                                          "if git stash list | grep -q \"temporary stash\"; then git stash pop; fi'"
+                        echo "Executing local git pull command: ${innerGitCmd}"
+                        try {
+                            sh innerGitCmd
+                        } catch (Exception e) {
+                            echo "Local git pull failed: ${e.getMessage()}. Continuing without updating."
+                        }
+
+                    }
+                }
             }
         }
 
