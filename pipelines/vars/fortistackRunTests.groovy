6a9fb195014389fce0542fc7f938017d7df1207e
Fosqa User <bruceyu777>
2025-07-03 23:37:38 -0700

    after each test, auto report oriole to mangoDB

--- pipelines/vars/fortistackRunTests.groovy
+++ pipelines/vars/fortistackRunTests.groovy
@@ -281,6 +281,8 @@ def call() {
                                       -e testcase/${SVN_BRANCH}/${params.FEATURE_NAME}/${params.TEST_CONFIG_CHOICE} \
                                       -g testcase/${SVN_BRANCH}/${params.FEATURE_NAME}/${group} \
                                       -d -s ${params.ORIOLE_SUBMIT_FLAG}
+                                    cd /home/fosqa/resources/tools
+                                    sudo /home/fosqa/resources/tools/venv/bin/python3 inject_autolib_result.py -r ${params.RELEASE} -g ${group}
                                 """
                             }
 
